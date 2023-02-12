f70161d82718d857e6e62da07838002e55222901
sandip2224 <sandipan2224@gmail.com>
2023-02-12 21:32:22 +0530

    Added spinner loading while fetching api data

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,22 +2,27 @@ import FeedbackItem from './FeedbackItem'
 import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext)
+import Spinner from './shared/Spinner'
 
-  if (!feedback || feedback.length === 0) {
+function FeedbackList() {
+  const { feedback, isLoading } = useContext(FeedbackContext)
+
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet!</p>
   }
-  return (
-    <div className='feedback-list'>
-      {feedback.map(item => {
-        return <FeedbackItem
-          key={item.id}
-          item={item}
-        />
-      })}
-    </div>
-  )
+
+  return isLoading ?
+    <Spinner />
+    : (
+      <div className='feedback-list'>
+        {feedback.map(item => {
+          return <FeedbackItem
+            key={item.id}
+            item={item}
+          />
+        })}
+      </div>
+    )
 }
 
 export default FeedbackList
